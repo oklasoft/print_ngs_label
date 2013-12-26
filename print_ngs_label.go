0b0c53bce8fb2cf94b63e341fcd1d672eead2465
Stuart Glenn <Stuart-Glenn@omrf.org>
2013-12-26 07:52:52 -0600

    Switches redis to pool & uses goroutine to save used IDs back to another set

--- print_ngs_label.go
+++ print_ngs_label.go
@@ -1,12 +1,31 @@
 package main
 
-import "github.com/garyburd/redigo/redis"
-import "os"
-import "os/exec"
-import "fmt"
-import "log"
+import (
+  "github.com/garyburd/redigo/redis"
+  "os"
+  "os/exec"
+  "fmt"
+  "log"
+  "flag"
+)
 
-func printLabel(r redis.Conn, num_ids int) {
+var (
+  hostname string
+  num_labels int
+)
+
+func init() {
+  flag.StringVar(&hostname,"host","localhost","Redis server hostname")
+  flag.IntVar(&num_labels,"labels",1,"Number of labels to print")
+
+  flag.Parse()
+}
+
+func save_used_label(p *redis.Pool, label string) {
+  redis.String(p.Get().Do("SADD", "ngs_ids_used_000", label))
+}
+
+func printLabel(p *redis.Pool, num_ids int) {
 
   lp := exec.Command("/bin/cat")
   outfile, err := os.Create("./out.txt")
@@ -26,12 +45,13 @@ func printLabel(r redis.Conn, num_ids int) {
   }
   
   for i:=0; i < num_ids; i++ {
-    reply, err := redis.String(r.Do("srandmember","ngs_ids_000"))
+    reply, err := redis.String(p.Get().Do("srandmember","ngs_ids_000"))
     if err != nil {
       log.Fatal(err)
     }
     fmt.Printf("ngs-%s\n",reply)
     lp_in.Write([]byte(fmt.Sprintf("ngs-%s\n",reply)))
+    go save_used_label(p,reply)
   }
 
   lp_in.Close()
@@ -40,11 +60,8 @@ func printLabel(r redis.Conn, num_ids int) {
 }
 
 func main() {
-  r, err := redis.Dial("tcp", "redis:6379")
-  if nil != err {
-    log.Fatal(err)
-  }
-  defer r.Close()
+  p := redis.NewPool(func() (redis.Conn, error) { return redis.Dial("tcp", fmt.Sprintf("%s:6379",hostname)) }, 2)
+  defer p.Close()
 
-  printLabel(r,1)
+  printLabel(p,num_labels)
 }
